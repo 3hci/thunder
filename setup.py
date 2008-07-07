9177436d164c6b0f95b57545c392477be5d9a551
Mike Partin <fuzzy@thwap.org>
2008-07-07 01:22:22 -0500

    added placeholder files for the rest of the library and the executable and modified setup.py appropriately

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 setup(name='thunder',
       version='0.1.0',
-      py_modules=['thunder/__init__', 'thunder/mslurp', 'thunder,parser'],
+      py_modules=['thunder/__init__', 'thunder/mslurp', 'thunder/parser','thunder/plugin','thunder/interface','thunder/iface_stdout','thunder/iface_dialog','thunder/iface_xdialog'],
 	  data_files=[('/sbin/', ['bin/installer.py']), ('/etc', ['etc/install.th'])]
       )
 
