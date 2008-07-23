14432e8cb677c85d1603fc40b065062531d0c9f3
fuzzy <fuzzy+gravatar@thwap.org>
2008-07-22 21:43:48 -0500

    fixed setup.py to include event.py

--- setup.py
+++ setup.py
@@ -2,7 +2,10 @@
 from distutils.core import setup
 setup(name='thunder',
       version='0.2.1',
-      py_modules=['thunder/__init__', 'thunder/slurp', 'thunder/net', 'thunder/engine'],
+      py_modules=[
+	  	'thunder/__init__', 'thunder/slurp', 'thunder/net', 'thunder/engine',
+		'thunder/event'
+	  ],
 	  data_files=[('/sbin/', ['bin/installer.py']), ('/etc', ['etc/install.th'])]
       )
 
