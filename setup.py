a80af850171020ea785e4e99963e05016dc4b882
fuzzy <fuzzy+gravatar@thwap.org>
2008-07-22 21:17:55 -0500

    moved installer.py to thunder/engine.py and added event.py am working on abstracting the parser out and making it easy for information displays to be written

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 from distutils.core import setup
 setup(name='thunder',
-      version='0.1.0',
-      py_modules=['thunder/__init__', 'thunder/slurp', 'thunder/net'],
+      version='0.2.1',
+      py_modules=['thunder/__init__', 'thunder/slurp', 'thunder/net', 'thunder/engine'],
 	  data_files=[('/sbin/', ['bin/installer.py']), ('/etc', ['etc/install.th'])]
       )
 
